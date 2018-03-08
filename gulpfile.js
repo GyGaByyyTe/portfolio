6f25166d205a8624330c6593c1db83a5a6f69d74
GyGaByyyTe <gygabyyyte@gmail.com>
2018-03-08 11:11:37 +0300

    stert template

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const pug = require('gulp-pug');
 
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 
 const del = require('del');
@@ -14,74 +15,82 @@ const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 
 const paths = {
-    root: './build',
-    templates: {
-        pages: 'src/templates/pages/*.pug',
-        src: 'src/templates/**/*.pug'
-    },
-    styles: {
-        src: 'src/styles/**/*.scss',
-        dest: 'build/assets/styles/'
-    },    
-    images: {
-        src: 'src/images/**/*.*',
-        dest: 'build/assets/images/'
-    },
-    scripts: {
-        src: 'src/scripts/**/*.js',
-        dest: 'build/assets/scripts/'
-    }
-}
+  root: './build',
+  templates: {
+    pages: 'src/templates/pages/*.pug',
+    src: 'src/templates/**/*.pug'
+  },
+  styles: {
+    src: 'src/styles/**/*.scss',
+    dest: 'build/assets/styles/'
+  },
+  images: {
+    src: 'src/images/**/*.*',
+    dest: 'build/assets/images/'
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'build/assets/scripts/'
+  }
+};
 
 // pug
 function templates() {
-    return gulp.src(paths.templates.pages)
-        .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest(paths.root));
+  return gulp
+    .src(paths.templates.pages)
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.root));
 }
 
 // scss
 function styles() {
-    return gulp.src('./src/styles/app.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(sourcemaps.write())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(paths.styles.dest))
+  return gulp
+    .src('./src/styles/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
 // очистка
 function clean() {
-    return del(paths.root);
+  return del(paths.root);
 }
 
 // webpack
 function scripts() {
-    return gulp.src('src/scripts/app.js')
-        .pipe(gulpWebpack(webpackConfig, webpack)) 
-        .pipe(gulp.dest(paths.scripts.dest));
+  return gulp
+    .src('src/scripts/app.js')
+    .pipe(gulpWebpack(webpackConfig, webpack))
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
 // галповский вотчер
 function watch() {
-    gulp.watch(paths.styles.src, styles);
-    gulp.watch(paths.templates.src, templates);
-    gulp.watch(paths.images.src, images);
-    gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.templates.src, templates);
+  gulp.watch(paths.images.src, images);
+  gulp.watch(paths.scripts.src, scripts);
 }
 
 // локальный сервер + livereload (встроенный)
 function server() {
-    browserSync.init({
-        server: paths.root
-    });
-    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+  browserSync.init({
+    server: paths.root
+  });
+  browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
 }
 
 // просто переносим картинки
 function images() {
-    return gulp.src(paths.images.src)
-        .pipe(gulp.dest(paths.images.dest));
+  return gulp.src(paths.images.src).pipe(gulp.dest(paths.images.dest));
 }
 
 exports.templates = templates;
@@ -89,8 +98,11 @@ exports.styles = styles;
 exports.clean = clean;
 exports.images = images;
 
-gulp.task('default', gulp.series(
+gulp.task(
+  'default',
+  gulp.series(
     clean,
     gulp.parallel(styles, templates, images, scripts),
     gulp.parallel(watch, server)
-));
+  )
+);
