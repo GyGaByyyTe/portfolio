49827932fd47dd9e0635616796b70d6e0687da96
GyGaByyyTe <gygabyyyte@gmail.com>
2018-03-26 01:56:10 +0300

    skills anim added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,29 +2,7 @@ var $ = require('jquery');
 
 require('./common/flipper');
 require('./common/blog');
-
-// $(document).ready(function(){
-//   $(".blog__aside-link").on("click", function (event) {
-//       event.preventDefault();
-//       var id  = $(this).attr('href');
-//       console.log(`${id}`);
-//       var top = $(`${id}`).offset().top;
-//       console.log(top);      
-//       $('body,html').animate({scrollTop: top}, 700);
-//   });
-// });
-
-// const activeArticleLink = document.querySelector('.blog__aside-item');
-// if (activeArticleLink ) {
-//   activeArticleLink.setAttribute(
-//     'class',
-//     'blog__aside-item blog__aside-item--active'
-//   );
-// } else {
-//   // не существует
-// }
-
-console.log('hello from JS');
+require('./common/skills');
 
 // menu overlay
 var sliderOverlay = $('#nav');
