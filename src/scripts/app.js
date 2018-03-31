0c727cb862f6e5c5516b75c4f5182ab4d8177331
GyGaByyyTe <gygabyyyte@gmail.com>
2018-03-31 21:02:56 +0300

    added loader

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,14 @@ require('./common/flipper');
 require('./common/blog');
 require('./common/skills');
 const googleMap = require('./common/map');
+const preloaderInit = require('./common/preloader');
+
+if (document.images.length > 0) {
+  preloaderInit();
+} else {
+  document.getElementById('js-preloader').classList.add('done');
+  document.body.style.overflow = 'auto';
+}
 
 // menu overlay
 var sliderOverlay = $('#nav');
